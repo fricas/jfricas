ed60336acc733cf1319f3c7efe6b8b99cf37bebf
Kurt Pagani <nilqed@gmail.com>
2019-10-10 23:14:08 +0200

    docker:runx

---
+++ docker/runx.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo Running "nilqed/jfricas:latest"
+echo Use docker commit if you want to save your changes!
+echo Warning: using xhost local:root
+xhost local:root
+docker run -ti --network=host --env DISPLAY=$DISPLAY nilqed/jfricas:latest jupyter notebook --no-browser --allow-root
+docker ps -a
+xhost -local:root
+echo done.
+
+
